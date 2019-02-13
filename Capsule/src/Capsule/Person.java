cc68d4fed9d1869af7a2c477b243af23ab41593b
kazuki7291 <uuddrlrlab@gmail.com>
2019-02-13 21:07:16 +0900

    0213

--- Capsule/src/Capsule/Person.java
+++ Capsule/src/Capsule/Person.java
@@ -1,8 +1,8 @@
 package Capsule;
 
 public class Person{
-	public String name = null;
-	public int age = 0;
+	private String name = null;
+	private int age = 0;
 	
 	public Person (String name, int age){
 		this.name=name;
@@ -17,6 +17,11 @@ public class Person{
 	public void setName(String name){
 		this.name= name;
 	}
-	
+	public int getAge(){
+		return this.age;
+	}
+	public void setAge(int age){
+		this.age= age;
+	}
 
 }
