0b7a78d4b2265fd760d7f6a409e1ae5a0f48b037
fujimon-san1996 <phantom_minds_0229@ezweb.ne.jp>
2019-03-15 21:42:25 +0900

    20190315

--- WebProj4/src/com/internousdev/webproj4/util/DBConnector.java
+++ WebProj4/src/com/internousdev/webproj4/util/DBConnector.java
@@ -12,7 +12,7 @@ public class DBConnector {
 	/**
 	 * データベース接続URL
 	 */
-	private static String url="jdbc:mysql;//localhost/testdb_fujimura4";
+	private static String url="jdbc:mysql://localhost/testdb_fujimura4";
 	/**
 	 * データベース接続ユーザ名
 	 */
